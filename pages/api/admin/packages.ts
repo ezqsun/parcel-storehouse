6308a4d3b6eba6e425a1a3cd7ad2e09ff92c0d6d
Mellie Vo <hello@mellie.dev>
2021-04-01 03:46:00 -0700

    Selection

--- pages/api/admin/packages.ts
+++ pages/api/admin/packages.ts
@@ -3,6 +3,34 @@ import { query } from "@lib/db";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => requireAdmin(req, res, async (req) => {
+
+  //Note: This is safe because the only parameter the user controls must be an int.
+  //      if it isn't it will be NaN; therefore, sanitized.
+  let sqlWhere = '';
+  const year = Number.parseInt(req.rawRequest.headers['year'] as string);
+  if (year != 0 && !Number.isNaN(year)) {
+    sqlWhere = `WHERE YEAR(packages.ordered_date)=${year}`
+  }
+
+  const custId = Number.parseInt(req.rawRequest.headers['cid'] as string);
+  if (custId != -1 && !Number.isNaN(custId)) {
+
+    if (sqlWhere) {
+      sqlWhere += ` AND packages.cid=${custId}`;
+    } else {
+      sqlWhere = `WHERE packages.cid=${custId}`
+    }
+    //
+  }
+
+  if ((req.rawRequest.headers['showbanned'] as string) === 'no') {
+    if (sqlWhere) {
+      sqlWhere += ' AND customers.is_blacklisted=0'
+    } else {
+      sqlWhere = 'WHERE customers.is_blacklisted=0'
+    }
+  }
+
   const packages = await query<any>(
     `SELECT
       packages.pid,
@@ -17,6 +45,7 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
         ON couriers.nid = packages.nid
       INNER JOIN customers
         ON customers.cid = packages.cid
+    ${sqlWhere} 
     ORDER BY packages.pid`);
 
   return {
